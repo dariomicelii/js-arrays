645f695b54848dea47967f15fbd1674ac4399583
dariomicelii <dariomiceli01@gmail.com>
2024-10-16 16:34:20 +0200

    first commit

---
+++ 01-arrays/script.js
@@ -0,0 +1,61 @@
+const teachers = [
+  'Nathan',
+  'Ed',
+  'Fabio',
+  'Phil',
+  'Carlo',
+  'Lewis',
+  'Luca'
+]; // NON MODIFICARE QUESTA VARIABILE
+
+// 1. Utilizzando un indice sull'array teachers, cambia il valore della
+// variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
+const fourthTeacher = teachers[3];
+
+// 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers.splice(4, 1, "Patrick")
+console.log(teachers);
+
+// 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
+const lastTeacher = teachers.pop();
+console.log(teachers);
+
+// 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
+const firstTeacher = teachers.shift();
+console.log(teachers);
+
+
+// 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa")
+console.log(teachers);
+
+// 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah")
+console.log(teachers);
+
+// 7. Verifica se 'Fabio' è presente nell'array teachers
+teachers.includes("Fabio")
+
+// e salva il risultato nella variabile isFabioPresent
+const isFabioPresent = teachers.includes("Fabio");
+console.log(isFabioPresent);
+
+// 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
+teachers.indexOf("Lewis")
+
+// e salvalo nella variabile lewisIndex
+const lewisIndex = teachers.indexOf("Lewis");
+console.log(lewisIndex);
+
+// 9. Unisci tutti gli insegnanti nell'array teachers in una stringa
+// separata da virgole e salvala nella variabile teachersString
+const teachersString = teachers.toString();
+console.log(teachersString)
+
+// 10. Verifica se l'array teachers è vuoto
+// e salva il risultato nella variabile isTeachersEmpty
+const isTeachersEmpty = teachers.length;
+console.log(isTeachersEmpty)                // Non sono sicuro che sia giusto,
+                                            // però stampando mi da il numero degli elementi 
+                                            // presenti nell'array, essendo diverso da 0 l'array
+                                            // non è vuoto!
